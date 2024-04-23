4d02bbc6e07631b9002ec3fb53772d22a1526145
王岑晗 <199524943@qq.com>
2024-04-23 18:51:30 +0800

    * 更新

--- Assets/GameMain/Mouse.cs
+++ Assets/GameMain/Mouse.cs
@@ -6,8 +6,25 @@ using UnityEngine.EventSystems;
 
 public class Mouse : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    Camera MainCamera;
-    Rigidbody rigidbody;
+    private Camera m_MainCamera;
+
+    Rigidbody2D rigidbody;
+
+    public List<Collider2D> Triggers = new List<Collider2D>();
+
+
+    Camera MainCamera
+    {
+        get
+        {
+            if (m_MainCamera == null)
+            {
+                m_MainCamera = GameObject.FindWithTag("MainCamera")?.GetComponent<Camera>();
+            }
+
+            return m_MainCamera;
+        }
+    }
 
     [ShowInInspector]
     Ray? ray;
@@ -18,73 +35,99 @@ public class Mouse : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, I
 
     public Vector3 TargetPosition;
 
+    public void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
 
     public void Update()
     {
-        if (IsEnter || IsDraging)
-        {
-            rigidbody.isKinematic = true;
-        }
-        else
-        {
-            rigidbody.isKinematic = false;
-        }
+
 
     }
 
+    public Vector3 DragOffset;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log($"OnTriggerEnter2D:{other}");
+        if (!Triggers.Contains(other))
+        {
+            Triggers.Add(other);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log($"OnTriggerExit2D:{other}");
+
+        if (Triggers.Contains(other))
+        {
+            Triggers.Remove(other);
+        }
+    }
 
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log($"OnBeginDrag");
-        IsDraging = true;
+        gameObject.layer = LayerMask.NameToLayer("Drag");
+        // rigidbody.simulated = false;
+        // IsDraging = true;
+        // rigidbody
 
-        MainCamera = GameObject.FindWithTag("MainCamera")?.GetComponent<Camera>();
-        ray = MainCamera.ScreenPointToRay(new Vector3(eventData.position.x, eventData.position.y, 0.1f));
-        // MainCamera.transform.TransformPoint(eventData.position)
-        // ray = new Ray(MainCamera.transform.position, MainCamera.transform.TransformDirection(MainCamera.transform.TransformPoint(eventData.position) * 3));
-        // Debug.Log($"OnDrag:{ray}");
-        if (Physics.Raycast(ray.Value, out RaycastHit hit, 3, LayerMask.GetMask("Table")))
-        {
-            Debug.Log($"Hit:{hit.collider.gameObject.name}");
-            TargetPosition = hit.point + (Vector3.up * 0.01f);
-            transform.position = hit.point + (Vector3.up * 0.01f);
+        // MainCamera = GameObject.FindWithTag("MainCamera")?.GetComponent<Camera>();
+        // ray = MainCamera.ScreenPointToRay(new Vector3(eventData.position.x, eventData.position.y, 0.1f));
+        // // MainCamera.transform.TransformPoint(eventData.position)
+        // // ray = new Ray(MainCamera.transform.position, MainCamera.transform.TransformDirection(MainCamera.transform.TransformPoint(eventData.position) * 3));
+        // // Debug.Log($"OnDrag:{ray}");
+        // if (Physics.Raycast(ray.Value, out RaycastHit hit, 3, LayerMask.GetMask("Table")))
+        // {
+        //     Debug.Log($"Hit:{hit.collider.gameObject.name}");
+        //     TargetPosition = hit.point + (Vector3.up * 0.01f);
+        //     transform.position = hit.point + (Vector3.up * 0.01f);
 
-        }
+        // }
 
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        MainCamera = GameObject.FindWithTag("MainCamera")?.GetComponent<Camera>();
-        ray = MainCamera.ScreenPointToRay(new Vector3(eventData.position.x, eventData.position.y, 0.1f));
-        // MainCamera.transform.TransformPoint(eventData.position)
+        transform.position = MainCamera.ScreenToWorldPoint(new Vector3(eventData.position.x, eventData.position.y, 10f)) + DragOffset;
+        // ray = MainCamera.ScreenPointToRay(new Vector3(eventData.position.x, eventData.position.y, 0.1f));
         // ray = new Ray(MainCamera.transform.position, MainCamera.transform.TransformDirection(MainCamera.transform.TransformPoint(eventData.position) * 3));
-        // Debug.Log($"OnDrag:{ray}");
-        if (Physics.Raycast(ray.Value, out RaycastHit hit, 3, LayerMask.GetMask("Table")))
-        {
-            Debug.Log($"Hit:{hit.collider.gameObject.name}");
-            transform.position = hit.point + (Vector3.up * 0.01f);
+        // // // Debug.Log($"OnDrag:{ray}");
+        // if (Physics.Raycast(ray.Value, out RaycastHit hit, 3, LayerMask.GetMask("Table")))
+        // {
+        //     Debug.Log($"Hit:{hit.collider.gameObject.name}");
+        //     transform.position = MainCamera.ScreenToWorldPoint(new Vector3(eventData.position.x, eventData.position.y, 0.1f));
 
-        }
-        else
-        {
-            Debug.Log($"Not Hit");
-        }
+
+        // }
+        // else
+        // {
+        //     Debug.Log($"Not Hit");
+        // }
 
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log($"OnEndDrag");
-        rigidbody.isKinematic = false;
-        IsDraging = false;
+        gameObject.layer = LayerMask.NameToLayer("Default");
+
+
+        // rigidbody.isKinematic = false;
+        // IsDraging = false;
 
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log($"OnPointerClick");
+        DragOffset = transform.position - MainCamera.ScreenToWorldPoint(new Vector3(eventData.position.x, eventData.position.y, 10f));
+
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -101,11 +144,6 @@ public class Mouse : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, I
     }
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rigidbody = GetComponent<Rigidbody>();
-    }
 
     // Update is called once per frame
 
